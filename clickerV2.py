772bee1b1ad4425d7ad4ce9cd05a14218db77c4e
Steph Poleij <99033734@mydavinci.nl>
2022-03-18 19:33:26 +0100

    clicker v2 af

--- clickerV2.py
+++ clickerV2.py
@@ -9,26 +9,41 @@ window.resizable(False, False)
 window.geometry('300x150')
 
 countNumber = 0
+prevColor = 'grey'
 
 def addOne():
     global countNumber
     countNumber += 1
-    intVar.set (countNumber)
+    intVar.set(countNumber)
     checkCount()
 
 def subtractOne():
     global countNumber
     countNumber -= 1
-    intVar.set (countNumber)
+    intVar.set(countNumber)
     checkCount()
 
 def checkCount():
+    global prevColor
     if countNumber == 0:
         window.configure(bg='grey')
+        prevColor = 'grey'
     elif countNumber >0:
         window.configure(bg='green')
+        prevColor = 'green'
     else:
         window.configure(bg='red')
+        prevColor = 'red'
+
+
+
+def onLabelEnter(event):
+    window.configure(bg='yellow')
+
+def onLabelLeave(event):
+    window.configure(bg=prevColor)
+
+
 
 options = {'fill': 'both', 'padx': 10, 'pady': 10, 'ipadx': 5}
 button1 = tkinter.Button(text='up', bg="white", fg="black", font=("Arial", 10), command=addOne)                      
@@ -37,6 +52,8 @@ button1.pack(options)
 intVar = tkinter.IntVar(value=0)
 countLabel = tkinter.Label(window)
 countLabel.configure(textvariable=intVar,bg="white", fg="black", font=("Arial", 10))
+countLabel.bind("<Enter>", onLabelEnter)
+countLabel.bind("<Leave>", onLabelLeave)
 
 countLabel.pack(options)
 
